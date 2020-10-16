5948bb1a01ab4c33c009f9c64314a156f1c58409
eslam4495 <eslammohamed4495@gmail.com>
2020-10-16 13:50:58 +0200

    add signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,7 @@ import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClientModule, HttpClient} from '@angular/common/http';
 import { LoginComponent } from './auth/login/login.component';
-
-
+import { SignupComponent } from './signup/signup.component';
 
 
 import {
@@ -64,6 +63,7 @@ export function createTranslateLoader(http: HttpClient) {
     AppComponent,
 	ChangePasswordComponent,
   ResetPasswordComponent,
+  SignupComponent,
   ForgetPasswordComponent,
   LoginComponent
   ],
